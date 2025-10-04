dee9920153978c86ded798da7fd9f00adbbb24dd
SyedEisarAli <sattarlabels@gmail.com>
2025-10-05 03:16:38 +0500

    Add files via upload

---
+++ contact-us.js
@@ -0,0 +1,104 @@
+// validation starts here
+
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+function validateName() {
+    var name = document.getElementById('contact-name').value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Write Full Name';
+        return false;
+
+    }
+    nameError.innerHTML = 'done';
+    return true;
+
+}
+
+function validatePhone() {
+    var phone = document.getElementById('contact-phone').value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone no is required';
+        return false;
+    }
+    if (phone.length !== 11) {
+        phoneError.innerHTML = 'Phone no should be 11 digits';
+        return false;
+    }
+    if (!phone.match(/^[0-9]{11}$/)) {
+        phoneError.innerHTML = 'only digits please';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+
+}
+
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+    if (email.lenght == 0) {
+        emailError.innerHTML = 'Email is Required'
+        return false
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email Invalid'
+        return false
+    }
+    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+
+
+}
+
+function validateMessage() {
+    var message = document.getElementById('contact-message').value;
+    var required = 20;
+    var left = required - message.length;
+    if (left > 0) {
+        messageError.innerHTML = left + ' More Characters Required';
+        return false;
+    }
+    messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+
+}
+
+function validateform() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fill all the Input field correctly';
+        setTimeout(function () { submitError.style.display = 'none'; }, 5000);
+        return false;
+
+    }
+}
+// validation ends here
+
+
+// local storage starts here
+
+function register() {
+    // event.preventDefault();
+    let name = document.querySelector("#contact-name").value;
+    let phone = document.querySelector("#contact-phone").value;
+    let emails = document.querySelector("#contact-email").value;
+
+
+    localStorage.setItem("Username", name);
+    localStorage.setItem("Phone", phone);
+    localStorage.setItem("Useremail", emails);
+}
+
+
+
+
+// local storage ends here
